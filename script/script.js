7fd6dc8bba0e4257c577b9a88c68ed3a225e387f
Ilya-Detkov <66683957+Ilya-Detkov@users.noreply.github.com>
2020-07-08 17:41:53 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -121,18 +121,24 @@ $(document).ready(function(){
 					},
 					submitHandler(form) {
 					let th = $(form);
-
-					$.ajax({
-					type: 'POST',
-					url: 'mail.php',
-					data: th.serialize(),
-				}).done(() => {
-
-					th.trigger('reset');
-				});
-
-				return false;
 				}
 			});
+    });
+    
+    //E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
 		});
+		return false;
+	});
 });
